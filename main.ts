b309cdde7c10565cd08281f8dff2b6b01cea2165
xthecapx <xthecapx@gmail.com>
2025-01-23 13:29:33 -0500

    update code and remove main.js from github

--- main.ts
+++ main.ts
@@ -1,15 +1,7 @@
-import { App, Plugin, PluginSettingTab, Setting, Modal } from 'obsidian';
+import { Plugin, Modal } from 'obsidian';
 import React, { StrictMode } from 'react';
 import { Root, createRoot } from 'react-dom/client';
 import { ReactView } from './ReactView';
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 class CvModal extends Modal {
 	root: Root | null = null;
@@ -47,64 +39,21 @@ class CvModal extends Modal {
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// Add a ribbon icon to open the modal
 		this.addRibbonIcon('pdf-file', 'Preview CV', () => {
 			new CvModal(this.app).open();
 		});
 
-		// Add command to open CV preview
 		this.addCommand({
-			id: 'open-cv-preview',
+			id: 'thecap-cv-generator-preview',
 			name: 'Open CV Preview',
 			callback: () => {
 				new CvModal(this.app).open();
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
